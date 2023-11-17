5f9e68186e051f2ed447d0f3f03155743b64235f
Jiajie Chen <c@jia.je>
2023-11-17 23:15:11 +0800

    Add multiple layers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import './App.css';
-import { Box, Button, Stack, Typography } from '@mui/material';
+import { Box, Button, Container, Grid, Paper, Stack, TextField, Typography } from '@mui/material';
 import { tomorrow as style } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
@@ -24,6 +24,12 @@ function AddRowMarker(code: string, line: number | undefined) {
 
 function App() {
   const [senderToReceiver1, setSenderToReceiver1] = useState<number[]>([]);
+  const [senderNetworkToDataLink1, setSenderNetworkToDataLink1] = useState<string[]>([]);
+  const [senderNetworkInput1, setSenderNetworkInput1] = useState("");
+
+  const sendNetwork1 = useCallback(() => {
+    setSenderNetworkToDataLink1(senderNetworkToDataLink1.concat(senderNetworkInput1));
+  }, [senderNetworkToDataLink1, senderNetworkInput1]);
 
   const [sender1Row, setSender1Row] = useState(2);
   const sender1 = AddRowMarker(`
@@ -46,8 +52,9 @@ function App() {
       setSender1Row(4);
     } else if (sender1Row === 4) {
       setSender1Row(5);
-    } else if (sender1Row === 5) {
+    } else if (sender1Row === 5 && senderNetworkToDataLink1.length > 0) {
       setSender1Row(6);
+      setSenderNetworkToDataLink1(senderNetworkToDataLink1.slice(1));
     } else if (sender1Row === 6) {
       setSender1Row(7);
     } else if (sender1Row === 7) {
@@ -56,11 +63,15 @@ function App() {
     } else if (sender1Row === 8) {
       setSender1Row(4);
     }
-  }, [sender1Row, senderToReceiver1]);
+  }, [sender1Row, senderToReceiver1, senderNetworkToDataLink1]);
 
   const canStepSender1 = useCallback(() => {
-    return true;
-  }, []);
+    if (sender1Row === 5 && senderNetworkToDataLink1.length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }, [sender1Row, senderNetworkToDataLink1]);
 
   const [receiver1Row, setReceiver1Row] = useState(2);
   const receiver1 = AddRowMarker(`
@@ -104,43 +115,81 @@ function App() {
   }, [receiver1Row, senderToReceiver1]);
 
   return (
-    <div className="App">
-      <Typography variant="h1">
+    <Container>
+      <Typography variant="h2">
         数据链路层协议
       </Typography>
       <Typography>
         在学习《计算机网络原理》课程时，数据链路层协议是一个重难点。为了理解《计算机网络》教材上讲述的数据链路层协议，你可以在本页面中观察各个数据链路层协议的工作方式。
       </Typography>
-      <Typography variant="h2">
+      <Typography variant="h3">
         协议一：乌托邦协议（Utopia）
       </Typography>
       <Typography>
         协议一提供了从发送方到接收方的单向数据传输。协议一假设了传输通道是无差错的，并且接收方可以任意快地处理输入数据。因此，发送方只需要循环发送数据，多快都可以。
       </Typography>
-      <Stack spacing={2} direction="row">
-        <Box>
-          <Typography>
-            协议一的发送方代码：
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Typography variant="h4">
+            发送方
           </Typography>
-          <SyntaxHighlighter language="javascript" style={style}>
-            {sender1}
-          </SyntaxHighlighter>
-          <Button variant="contained" onClick={stepSender1} disabled={!canStepSender1()}>下一步</Button>
-        </Box>
-        <Typography>
-          {senderToReceiver1}
-        </Typography>
-        <Box>
-          <Typography>
-            协议一的接收方代码：
+          <Box>
+            <Typography variant="h5">
+              网络层
+            </Typography>
+            <TextField label="Input" variant="outlined" fullWidth onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setSenderNetworkInput1(event.target.value);
+            }} />
+            <Button variant="contained" onClick={sendNetwork1}>发送</Button>
+            {senderNetworkToDataLink1}
+          </Box>
+          <Box>
+            <Typography variant="h5">
+              数据链路层
+            </Typography>
+            <Typography>
+              协议一的发送方代码：
+            </Typography>
+            <SyntaxHighlighter language="javascript" style={style}>
+              {sender1}
+            </SyntaxHighlighter>
+            <Button variant="contained" onClick={stepSender1} disabled={!canStepSender1()}>下一步</Button>
+          </Box>
+          <Box>
+            <Typography variant="h5">
+              物理层
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h2">
+            接收方
           </Typography>
-          <SyntaxHighlighter language="javascript" style={style}>
-            {receiver1}
-          </SyntaxHighlighter>
-          <Button variant="contained" onClick={stepReceiver1} disabled={!canStepReceiver1()}>下一步</Button>
-        </Box>
-      </Stack>
-    </div>
+          <Box>
+            <Typography variant="h3">
+              网络层
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h3">
+              数据链路层
+            </Typography>
+            <Typography>
+              协议一的接收方代码：
+            </Typography>
+            <SyntaxHighlighter language="javascript" style={style}>
+              {receiver1}
+            </SyntaxHighlighter>
+            <Button variant="contained" onClick={stepReceiver1} disabled={!canStepReceiver1()}>下一步</Button>
+          </Box>
+          <Box>
+            <Typography variant="h3">
+              物理层
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
